7ea9195a0a2f30b450d9249283d2d16a0468c12d
Jonathan B. Koo-Lau <62316701+jondoglover@users.noreply.github.com>
2020-08-16 12:09:49 -0700

    Create opentab.js

---
+++ scripts/opentab.js
@@ -0,0 +1,16 @@
+function openTab(tabName,elmnt,color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(tabName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+
+}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
