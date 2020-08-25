bc753fbbb05efd2f99415ae2c8e2027b0b13021e
ckn <68473617+ch1ck3n-byte@users.noreply.github.com>
2020-08-24 21:02:31 -0600

    sigh

--- scripts/opentab.js
+++ scripts/opentab.js
@@ -12,15 +12,21 @@ function openTab(tabName,elmnt,color) {
   }
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of ce259c5... Very much sigh why am i even doing this
   document.getElementById(tabName).style.display = "block";
 <<<<<<< HEAD
 =======
   elmnt.style.backgroundColor = color;
 >>>>>>> parent of 76cce81... remove changecolor element from openTab function
 }
+<<<<<<< HEAD
 =======
 >>>>>>> parent of 0c58715... Revert "remove changecolor element from openTab function"
 =======
 >>>>>>> parent of 0c58715... Revert "remove changecolor element from openTab function"
+=======
+>>>>>>> parent of ce259c5... Very much sigh why am i even doing this
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
