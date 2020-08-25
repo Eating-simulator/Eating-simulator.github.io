14aeba62bebe66c8baeb30b1e38eb0135c54c18b
Jonathan B. Koo-Lau <62316701+jondoglover@users.noreply.github.com>
2020-08-24 21:22:27 -0500

    Create colorswitch.js

---
+++ scripts/colorswitch.js
@@ -0,0 +1,11 @@
+var mode = 0
+
+function triggerColors() {
+  if (mode == 0) {mode = 1}
+  else if {mode = 0}
+  if (mode == 0) {
+    document.body.style = 'color:black;background-color:white;';
+  } else {
+    document.body.style = 'color:white;background-color:black;';
+  }
+}
