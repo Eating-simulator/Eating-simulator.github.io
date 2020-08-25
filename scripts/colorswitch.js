4b8c9a089721e7e65b8794d1c8ba99797191b09c
ckn <68473617+ch1ck3n-byte@users.noreply.github.com>
2020-08-24 20:28:34 -0600

    Update colorswitch.js

--- scripts/colorswitch.js
+++ scripts/colorswitch.js
@@ -1,8 +1,7 @@
 var mode = 0
 
 function triggerColors() {
-  if (mode == 0) {mode = 1}
-  else {mode = 0}
+  if (mode == 0) {mode = 1} else {mode = 0}
   if (mode == 0) {
     document.body.style = 'color:black;background-color:white;';
   } else {
