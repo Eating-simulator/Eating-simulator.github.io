7a998117d0141f023c079617060bc601814ae8b9
Jonathan B. Koo-Lau <62316701+jondoglover@users.noreply.github.com>
2020-08-25 09:59:08 -0500

    Update colorswitch.js

--- scripts/colorswitch.js
+++ scripts/colorswitch.js
@@ -4,9 +4,11 @@ function triggerColors() {
   if (mode == 0) {mode = 1} else {mode = 0}
   if (mode == 0) {
     document.body.style = 'color:black;background-color:white;';
-    document.querySelector("a").style='color:yellow;background-color:gray';
+    document.querySelector("a").style='color:yellow;';
+    document.querySelector("a").onmouseover=`document.querySelector('a').style.color = 'gray'`;
   } else {
     document.body.style = 'color:white;background-color:black;';
-    document.querySelector("a").style='color:yellow;background-color:gray';
+    document.querySelector("a").style='color:blue;';
+    document.querySelector("a").onmouseover=`document.querySelector('a').style.color = 'yellow'`;
   }
 }
