9c8d23f8a21daac1dbb9d9e0618bff74bf69ff2f
ckn <68473617+ch1ck3n-byte@users.noreply.github.com>
2020-08-25 09:28:51 -0600

    Revert "Update opentab.js"
    This reverts commit 9322e987824fe7afdc8ad753089391fe6f23ba31.

--- scripts/opentab.js
+++ scripts/opentab.js
@@ -10,4 +10,8 @@ function openTab(tabName,elmnt,color) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].style.backgroundColor = "";
   }
+  document.getElementById(tabName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
 }
