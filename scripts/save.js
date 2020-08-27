d14e1be232855e41b653b7ab5c642a3bbbe8c2ca
Jonathan B. Koo-Lau <62316701+jondoglover@users.noreply.github.com>
2020-08-27 14:07:58 -0500

    Update save.js

--- scripts/save.js
+++ scripts/save.js
@@ -1,14 +1,6 @@
 // For saving and loading
 // Has error, do not work it yet
 
-thevariables = ['money', 'calories', 'cheese', 'dorito', 'buttontime', 'worldmap', 'lockeddoor', 'soldierbuttonpressed', 'fortressdoorclicked', 'chefapron', 'whynot', 'libraryKey', 'fWM', 'popularity', 'howpopular', 'moneypersecond', 'calDec', 'lotterymachinemoney', 'lotterymachineturnedon'];
-
-function strToOther(string) {
-    if (typeof string == 'string') {
-        return 'string'
-    }
-}
-
 function checkNull(variable) {
     if (localStorage.getItem(variable) == null) {
         return true;
@@ -17,10 +9,12 @@ function checkNull(variable) {
     }
 }
 
-for (variable in thevariables) {
-    if (checkNull(variable)) {
-        localStorage.setItem(variable, variable);
+function checkSave() {
+    if (localStorage.getItem('data') == null) {
+        localStorage.setItem('data', data);
     } else {
-        variable = localStorage.getItem(variable);
+        data = localStorage.getItem('data');
     }
 }
+
+checkSave();
