94d257c1ccdfc1ecc02d21b09ee8ac6b8ce02133
ckn <68473617+ch1ck3n-byte@users.noreply.github.com>
2020-08-26 18:24:07 -0600

    Update colorswitch.js

--- scripts/colorswitch.js
+++ scripts/colorswitch.js
@@ -1,16 +1,7 @@
 var mode = 0
 
 function triggerColors() {
-  if (mode == 0) {mode = 1} else {mode = 0}
-  if (mode == 0) {
-    document.body.style = 'color:black;background-color:white;';
-    document.querySelector("a").style='color:yellow;';
-    document.querySelector("a").onmouseover=`document.querySelector('a').style.color = 'gray'`;
-    document.getElementById("output").style = 'border-color: white';
-  } else {
-    document.body.style = 'color:white;background-color:black;';
-    document.querySelector("a").style='color:blue;';
-    document.querySelector("a").onmouseover=`document.querySelector('a').style.color = 'yellow'`;
-    document.getElementById("output").style = 'border-color: black';
-  }
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+  document.querySelector(".output").className = "output-white"
 }
