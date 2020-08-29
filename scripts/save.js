c609351ce80d727c315a6eb8dcc04dac879097b0
Jonathan B. Koo-Lau <62316701+jondoglover@users.noreply.github.com>
2020-08-29 14:46:09 -0500

    Update save.js

--- scripts/save.js
+++ scripts/save.js
@@ -1,15 +1,18 @@
 // For saving and loading
 // Has error, do not work it yet
 
-function checkSave() {
+function loadOrSave() {
     if (localStorage.getItem('data')) {
         data = JSON.parse(localStorage.getItem('data'));
     } else {
-        localStorage.setItem('data',JSON.stringify(data));
+        save()
     }
+}
+function save() {
+    localStorage.setItem("data", JSON.stringify(data));
 }
 /*
 * if data in local storage is not null then load the data into data variable
 * else save in local storage
 */
-checkSave();
+loadOrSave();
