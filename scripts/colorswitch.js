e9ff03e46c889b9a3c3a2147008940e4f507027b
ckn <68473617+ch1ck3n-byte@users.noreply.github.com>
2020-08-30 09:37:43 -0600

    Update colorswitch.js

--- scripts/colorswitch.js
+++ scripts/colorswitch.js
@@ -3,5 +3,11 @@ var mode = 0
 function triggerColors() {
    var element = document.body;
    element.classList.toggle("dark-mode");
+  if (mode == 0) {
   document.querySelector(".output").className = "output-white"
+  mode = 1
+  } else {
+   document.querySelector(".output").className = "output"
+   mode = 0
+  }
 }
