e6c1fac664703bf04b8970d3872c028375a5582e
Jonathan B. Koo-Lau <62316701+jondoglover@users.noreply.github.com>
2020-08-30 10:51:09 -0500

    Update colorswitch.js

--- scripts/colorswitch.js
+++ scripts/colorswitch.js
@@ -1,13 +1,7 @@
-var mode = 0
-
 function triggerColors() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
-  if (mode == 0) {
-  document.querySelector(".output").className = "output-white"
-  mode = 1
-  } else {
-   document.querySelector(".output").className = "output"
-   mode = 0
-  }
+   if (white) {
+      white = false;
+   } else {
+      white = true;
+   }
 }
