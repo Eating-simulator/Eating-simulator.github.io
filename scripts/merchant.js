297941989cfaca36e3410a8514b489dcf6738d37
ckn <68473617+ch1ck3n-byte@users.noreply.github.com>
2020-08-31 13:51:23 -0600

    Create merchant.js

---
+++ scripts/merchant.js
@@ -0,0 +1,15 @@
+var merchant = {
+  setmechantspeech : function(speech) {
+  document.querySelector(".merchantspeech").innerHTML = speech;
+  }
+  givemesomething : function() {
+    var random = Math.floor(Math.random() * 11);
+    if (random == 0) {
+    merchant.setmerchantspeech("Here's a jumper flavored doggo! <br> They taste good.")
+    } else if (random == 1) {
+    merchant.setmerchantspeech("Here's a watermelon flavored dorito. <br> i never tried them, but i'm sure they taste good.")
+    } else if (random == 2) {
+    merchant.setmerchantspeech("Here's a lemon! eat them, your mouth will hurt. <br> but i think you can squirt the juice to a mob <br> and it'll get easier to defeat it.")
+    }
+  }
+}
