bc8829c970abfb4f4746945ba289bb35b8b72267
ckn <68473617+ch1ck3n-byte@users.noreply.github.com>
2020-09-01 10:30:10 -0600

    Update merchant.js

--- scripts/merchant.js
+++ scripts/merchant.js
@@ -6,21 +6,21 @@ var merchant = {
   givemesomething : function() {
     random = Math.floor(Math.random() * 8);
     if (random == 0) {
-    merchant.setmerchantspeech("Here's a jumper flavored doggo! <br> They taste good.")
+    merchant.setmerchantspeech("Here's a jumper flavored doggo!   They taste good.")
     } else if (random == 1) {
-    merchant.setmerchantspeech("Here's a watermelon flavored dorito. <br> i never tried them, but i'm sure they taste good.")
+    merchant.setmerchantspeech("Here's a watermelon flavored dorito.   i never tried them, but i'm sure they taste good.")
     } else if (random == 2) {
-    merchant.setmerchantspeech("Here's a lemon! eat them, your mouth will hurt. <br> but i think you can squirt the juice to a mob <br> and it'll get easier to defeat it.")
+    merchant.setmerchantspeech("Here's a lemon! eat them, your mouth will hurt.   but i think you can squirt the juice to a mob   and it'll get easier to defeat it.")
     } else if (random == 3) {
-     merchant.setmerchantspeech("Here's a headphone! <br> wait, why am i giving you a headphone?") 
+     merchant.setmerchantspeech("Here's a headphone!   wait, why am i giving you a headphone?") 
     } else if (random == 4) {
-     merchant.setmerchantspeech("Here you go! it's a pumpkin flavoured apple pie. <br> now that just doesn't make sense.") 
+     merchant.setmerchantspeech("Here you go! it's a pumpkin flavoured apple pie.   now that just doesn't make sense.") 
     } else if (random == 5) {
      merchant.setmerchantspeech("Ahh, let me give you one of my favorites. it's a dorito!") 
     } else if (random == 6) {
      merchant.setmerchantspeech("It's a dorito!") 
     } else if (random == 7) {
-     merchant.setmerchantspeech("Here is a scrumticiolamy-yuatizin lollipop! <br> it's actually a thing.") 
+     merchant.setmerchantspeech("Here is a scrumticiolamy-yuatizin lollipop!   it's actually a thing.") 
     }
   }
 }
