8a54e7e14d534e700123e44540dee447471ad5d1
Jonathan B. Koo-Lau <62316701+jondoglover@users.noreply.github.com>
2020-08-31 19:24:04 -0500

    Update mexmia.js

--- scripts/mexmia.js
+++ scripts/mexmia.js
@@ -1,1 +1,17 @@
 // mexmia's code
+
+var choice;
+var mmStage = 0;
+
+function mexmiaChoice(side) {
+  choice = side;
+}
+
+function mexmia() {
+  var mexmia = document.getElementById("mexmiaSpeak");
+  switch (mmStage) {
+    case 0:
+      if (choice == 'left') {
+        mexmia.innerHTML = ''
+  }
+}
