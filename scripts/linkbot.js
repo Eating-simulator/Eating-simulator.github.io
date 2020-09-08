c64c1df80a479636bfefdc4e2ec64bf12d742a6b
Jonathan B. Koo-Lau <62316701+jondoglover@users.noreply.github.com>
2020-09-08 15:27:22 -0700

    Update linkbot.js

--- scripts/linkbot.js
+++ scripts/linkbot.js
@@ -6,7 +6,7 @@ var linkbot = {
 	insult : function() {
 		texts= ["hey! how dare you!", "Grr.. i'm mad!", "I summon all my soldiers to attack you!", "Stoppit! you're annoying me!", "AHHAHHHAHHHH! I'M MAD!", "I might take all of your links away if you don't stop."];
 		text = texts[Math.floor(Math.random() * texts.length)];
-		linkbot.setspeech(speech);
+		linkbot.setspeech(this.speech);
 
 	}
 
