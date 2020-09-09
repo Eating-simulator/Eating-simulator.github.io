6f39908ca31b388fd93c79e89db056685dfd76d1
ckn <68473617+ch1ck3n-byte@users.noreply.github.com>
2020-09-08 19:53:38 -0600

    Update linkbot.js

--- scripts/linkbot.js
+++ scripts/linkbot.js
@@ -1,12 +1,11 @@
 var linkbot = {
-	speech : document.querySelector(".linkbotspeech"),
 	setspeech : function(text) {
-		linkbot.speech.innerHTML = text;
+		document.querySelector(".linkbotspeech").innerHTML = text;
 	},
 	insult : function() {
 		texts= ["hey! how dare you!", "Grr.. i'm mad!", "I summon all my soldiers to attack you!", "Stoppit! you're annoying me!", "AHHAHHHAHHHH! I'M MAD!", "I might take all of your links away if you don't stop."];
 		text = texts[Math.floor(Math.random() * texts.length)];
-		linkbot.setspeech(this.speech);
+		linkbot.setspeech(text);
 
 	}
 
