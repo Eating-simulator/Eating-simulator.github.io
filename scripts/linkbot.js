0dc4f46cbc7652492fa3be90b7e2b8b603dab351
Jonathan B. Koo-Lau <62316701+jondoglover@users.noreply.github.com>
2020-09-08 20:05:20 -0700

    turned linkbot.js into complicated code and added insultValue

--- scripts/linkbot.js
+++ scripts/linkbot.js
@@ -1,12 +1,20 @@
 var linkbot = {
+	insultLevel : 0,
 	setspeech : function(text) {
 		document.querySelector(".linkbotspeech").innerHTML = text;
 	},
 	insult : function() {
-		texts= ["hey! how dare you!", "Grr.. i'm mad!", "I summon all my soldiers to attack you!", "Stoppit! you're annoying me!", "AHHAHHHAHHHH! I'M MAD!", "I might take all of your links away if you don't stop."];
-		text = texts[Math.floor(Math.random() * texts.length)];
-		linkbot.setspeech(text);
-
+		texts= [
+			{text:"hey! how dare you!",insultValue:1},
+			{text:"Grr.. i'm mad!",insultValue:2},
+			{text:"I summon all my soldiers to attack you!",insultValue:2},
+			{text:"Stoppit! you're annoying me!",insultValue:2},
+			{text:"AHHAHHHAHHHH! I'M MAD!",insultValue:2},
+			{text:"I might take all of your links away if you don't stop.",insultValue:1},
+		];
+		text2 = texts[Math.floor(Math.random() * 4)];
+		linkbot.setspeech(text2.text);
+		this.insultLevel += text2.insultValue;
 	}
 
 
