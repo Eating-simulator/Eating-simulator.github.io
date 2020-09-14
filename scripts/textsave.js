382d88dd34102637bff531f693e2829294abfeb3
ckn <68473617+ch1ck3n-byte@users.noreply.github.com>
2020-09-14 15:30:22 -0600

    Дополнительный уровень защиты
    Дополнительный уровень защиты

--- scripts/textsave.js
+++ scripts/textsave.js
@@ -1,5 +1,5 @@
 var textsave = {
 	save : function() {
-		document.getElementById("textareasave").innerHTML = JSON.stringify(data);
+		document.getElementById("textareasave").innerHTML = btoa(btoa(JSON.stringify(data)));
 	}
 }
