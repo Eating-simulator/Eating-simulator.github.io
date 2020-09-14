8a043326e734332c0e06ce42723ce96200b9fe77
ckn <68473617+ch1ck3n-byte@users.noreply.github.com>
2020-09-14 16:54:50 -0600

    Добавлена погрузочная часть (Огромный шанс ES Получение чрезвычайно взломали)

--- scripts/textsave.js
+++ scripts/textsave.js
@@ -1,5 +1,8 @@
 var textsave = {
 	save : function() {
 		document.getElementById("textareasave").innerHTML = btoa(btoa(JSON.stringify(data)));
+	},
+	load : function() {
+		data = atob(atob(document.getElementById("textareasave").value));
 	}
 }
