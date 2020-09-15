54e2734716394439c0de88c30b46134d241acf15
ckn <68473617+ch1ck3n-byte@users.noreply.github.com>
2020-09-15 08:36:24 -0600

    Теперь сохранение текста должно работать. Серьёзно!

--- scripts/textsave.js
+++ scripts/textsave.js
@@ -3,6 +3,6 @@ var textsave = {
 		document.getElementById("textareasave").innerHTML = btoa(btoa(JSON.stringify(data)));
 	},
 	load : function() {
-		data = atob(atob(document.getElementById("textareasave").value));
+		data = JSON.parse(atob(atob(document.getElementById("textareasave").value)));
 	}
 }
