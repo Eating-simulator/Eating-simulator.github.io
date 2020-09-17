487989ad5b198cf0c4855d932f3b731b317840bd
ckn <68473617+ch1ck3n-byte@users.noreply.github.com>
2020-09-17 14:33:10 -0600

    Update mexmia.js

--- scripts/mexmia.js
+++ scripts/mexmia.js
@@ -1,18 +1,35 @@
-// mexmia's code
-
-var choice;
-var mmStage = 0;
-
-function mexmiaChoice(side) {
-  choice = side;
-}
-
-function mexmia() {
-  var mexmia = document.getElementById("mexmiaSpeak");
-  switch (mmStage) {
-    case 0:
-      if (choice == 'left') {
-        mexmia.innerHTML = ''
-  }
+var mexmiasteps = -1
+function mexmiaChoice(leftorright) {
+  document.getElementById("mexmiastart").style.display = "none";
+  var mexmiahtml = document.getElementById("mexmiaSpeak");
+  var mexmiaLeft = document.getElementById("mexmiaLeft");
+  var mexmiaRight = document.getElementById("mexmiaRight");
+    if (mexmiasteps == -1){
+    if (leftorright == "left"){
+      mexmiahtml.innerHTML = "I am Mexmia the Terrible Unicorn!"
+    } else {
+      mexmiastartover()
+      return;
+    }
+  }else if (mexmiasteps == 0){
+    if (leftorright == "left"){
+      mexmiahtml.innerHTML = "Hmmm.. i don't know! \n Second question: if E Implies A And A Implies T, What does T imply?"
+      mexmiaLeft.innerHTML = "Of course, that would be the pizza scroll"
+      mexmiaRight.innerHTML = "I!"
+      mexmiasteps++;
+    } else {
+      mexmiastartover()
+      return;
+    }
+  }else if (mexmiasteps == 1) {
+    if (leftorright == "right"){
+      mexmiahtml.innerHTML = "That's correct! Now, Who made eating simulator?"
+      mexmiaLeft.innerHTML = "Bill gates"
+      mexmiaRight.innerHTML = "ID7, Techonodogg, ch1ck3n."
+      mexmiasteps++;
+    } else {
+      mexmiastartover()
+      return;
+    }
 }
 }
