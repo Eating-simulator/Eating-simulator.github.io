80e70a551a2cf26809c324e836d8772206b3e52e
ckn <68473617+ch1ck3n-byte@users.noreply.github.com>
2020-09-17 18:04:12 -0600

    Update textsave.js

--- scripts/textsave.js
+++ scripts/textsave.js
@@ -1,8 +1,1 @@
-var textsave = {
-	save : function() {
-		document.getElementById("textareasave").innerHTML = btoa(btoa(JSON.stringify(data)));
-	},
-	load : function() {
-		data = JSON.parse(atob(atob(document.getElementById("textareasave").value)));
-	}
-}
+var textsave={save:function(){document.getElementById("textareasave").innerHTML=btoa(btoa(JSON.stringify(data)))},load:function(){data=JSON.parse(atob(atob(document.getElementById("textareasave").value)))}};
