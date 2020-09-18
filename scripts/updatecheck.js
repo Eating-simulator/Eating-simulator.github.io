98b7af15cfb4633d5c58375bb7ad5b9578a3d277
ckn <68473617+ch1ck3n-byte@users.noreply.github.com>
2020-09-18 09:04:36 -0600

    Delete updatecheck.js

--- scripts/updatecheck.js
+++
@@ -1,9 +0,0 @@
-var george = new XMLHttpRequest();
-george.open("GET", "https://eating-simulator.github.io/credits.JSON", false);
-var olddata = JSON.parse(george.responseText).data;
-setInterval(function (){
-george.open("GET", "https://eating-simulator.github.io/credits.JSON", false);
-if (george.responceText != olddata){
-  alert("A new update is available. much reload?")
-}
-}, 1000)
