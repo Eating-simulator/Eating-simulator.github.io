11055e71a8949ed1c74594da40db247c1c06228b
ckn <68473617+ch1ck3n-byte@users.noreply.github.com>
2020-09-18 08:59:11 -0600

    Update updatecheck.js

--- scripts/updatecheck.js
+++ scripts/updatecheck.js
@@ -1,8 +1,8 @@
 var george = new XMLHttpRequest();
-george.open("GET", "https://eating-simulator.github.io/credits.JSON" false);
+george.open("GET", "https://eating-simulator.github.io/credits.JSON", false);
 var olddata = JSON.parse(george.responseText).data;
 setInterval(function (){
-george.open("GET", "https://eating-simulator.github.io/credits.JSON" false);
+george.open("GET", "https://eating-simulator.github.io/credits.JSON", false);
 if (george.responceText != olddata){
   alert("A new update is available. much reload?")
 }
