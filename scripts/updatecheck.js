0cd96fcfc83977bfc2cf7d7d74c0fe0f0f2e5879
ckn <68473617+ch1ck3n-byte@users.noreply.github.com>
2020-09-18 08:52:28 -0600

    Create updatecheck.js

---
+++ scripts/updatecheck.js
@@ -0,0 +1,9 @@
+var george = new XMLHttpRequest();
+george.open("GET", "https://eating-simulator.github.io/credits.JSON" false);
+var olddata = JSON.parse(george.responseText).data;
+setInterval(function (){
+george.open("GET", "https://eating-simulator.github.io/credits.JSON" false);
+if (george.responceText != olddata){
+  alert("A new update is available. much reload?")
+}
+}, 1000)
