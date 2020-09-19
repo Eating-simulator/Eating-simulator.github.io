e3bf66f9ac833631d397fdd565128700a42100ea
ckn <68473617+ch1ck3n-byte@users.noreply.github.com>
2020-09-19 11:59:35 -0600

    Update google.js

--- scripts/google.js
+++ scripts/google.js
@@ -1,14 +1,14 @@
 var googleuser = {
-email : user.getEmail(),
-username : user.getName(),
-image : user.getImageUrl()
+email : user.getBasicProfile().getEmail(),
+username : user.getBasicProfile().getBasicProfile().getName(),
+image : user.getBasicProfile().getImageUrl()
 }
   function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
     });
   }
-if (googleuser.email == null) {
+if (user.getBasicProfile().getEmail() == null) {
   document.getElementById("GoogleUserStatus").innerHTML = "User not signed in. such sad."
 }else {
   document.getElementById("GoogleUserStatus").innerHTML = "Signed in as " + googleuser.username + ". <a onClick='signOut()'>Sign out?</a>"
