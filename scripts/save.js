3729cccbe00c39da9704b9b276bd23e315ea7ebe
ckn <68473617+ch1ck3n-byte@users.noreply.github.com>
2020-10-11 14:43:45 -0600

    This is for a bug fix

--- scripts/save.js
+++ scripts/save.js
@@ -1,5 +1,20 @@
 // For saving and loading
 // Has error, do not work it yet
+function convertFromHex(hex) {
+    var hex = hex.toString();//force conversion
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+}
+
+function convertToHex(str) {
+    var hex = '';
+    for(var i=0;i<str.length;i++) {
+        hex += ''+str.charCodeAt(i).toString(16);
+    }
+    return hex;
+}
 function loadOrSave() {
     if (localStorage.getItem('data')) {
         data = JSON.parse(converFromHex(localStorage.getItem('data')));
