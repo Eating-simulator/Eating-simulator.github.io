650e0164ec3fb16f4c5f2b4839d693919ea295c1
Landon flop <admin@ch1ck3n.com>
2020-10-11 20:49:51 +0000

    Minify everything

--- scripts/textsave.js
+++ scripts/textsave.js
@@ -1,8 +1,1 @@
-var textsave = {
-	save: function () {
-		document.getElementById("textareasave").innerHTML = convertToHex(btoa(btoa(JSON.stringify(data))))
-	},
-	load: function () {
-		data = JSON.parse(atob(atob(convertFromHex(document.getElementById("textareasave").value))))
-	}
-};
+var textsave={save:function(){document.getElementById("textareasave").innerHTML=convertToHex(btoa(btoa(JSON.stringify(data))))},load:function(){data=JSON.parse(atob(atob(convertFromHex(document.getElementById("textareasave").value))))}};
